2a2d56eb1e253e7db0c1ebc82806ed305555ee92
Everton Dalla Corte <etodalla@gmail.com>
2025-08-14 16:21:32 -0300

    feat: altera pagina padrão do laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('body')
+    <div class="container">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main content of the home page.</p>
+    </div>
+@endsection
