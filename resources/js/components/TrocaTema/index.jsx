e8415b41695d41e16c32aca4fd26707028b2d5d6
Everton Dalla Corte <etodalla@gmail.com>
2025-08-15 20:01:56 -0300

    refactor(components): altera classes do TrocaTema para novo padrão

--- resources/js/components/TrocaTema/index.jsx
+++ resources/js/components/TrocaTema/index.jsx
@@ -1,25 +1,26 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
 
-let temaAtual = localStorage.getItem('tema') || 'ligth';
+let temaAtual = localStorage.getItem('theme') || 'ligth';
 
 export function TrocaTema() {
     const [tema, setTema] = useState(temaAtual);
 
     const alteraTemaSite = () => {
         setTema(tema === 'dark' ? 'ligth' : 'dark');
-        trocaTema();
+        setTheme(tema === 'dark' ? 'ligth' : 'dark');
     }
 
     return (
-        <button
-            onClick={() => alteraTemaSite()}
-            className='p-1 text-blue-500 rounded-full bg-white dark:bg-slate-800 border-1 fixed top-5 right-5 cursor-pointer hover:bg-gray-700'
-        >
-            {tema === 'dark' && <SunIcon className="size-4"/>}
-            {tema === 'ligth' && <MoonIcon className="size-4"/>}
-        </button>
+        <div className='position-absolute'>
+            <button
+                onClick={() => alteraTemaSite()}
+                className='position-fixed top-0 end-0 m-2 btn btn-outline-primary btn-sm rounded-circle'
+            >
+                {tema === 'dark' && <i className="bi bi-brightness-high"></i>}
+                {tema === 'ligth' && <i className="bi bi-moon-stars"></i>}
+            </button>
+        </div>
     );
 }
 
