be8df7cbce2f8084625ed3a4d64a3bfb1c94e4c7
Everton Dalla Corte <etodalla@gmail.com>
2025-08-15 00:22:58 -0300

    feat(service): implementa WeatherStackService para obter clima atual

---
+++ app/Services/WeatherStackService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class WeatherStackService
+{
+    protected string $baseUrl;
+    protected string $apiKey;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://api.weatherstack.com';
+        $this->apiKey = config('services.weatherstack.key');
+    }
+
+    /**
+     * Retorna o clima atual com base em cidade ou coordenadas.
+     */
+    public function getCurrentWeather(array $query)
+    {
+        $params = [
+            'access_key' => $this->apiKey,
+            'units' => 'm',
+            'language' => config('app.locale'),
+            'query' => $query,
+        ];
+
+        $response = Http::get($this->baseUrl.'/current', $params);
+
+        if ($response->failed()) {
+            return null;
+        }
+
+        return $response->json();
+    }
+}
